a0963df29542f31ae83614e4b7ab3b57bef0e69d
MadInc63 <MadInc63@gmail.com>
2017-11-27 18:11:00 +0300

    zzz

--- coursera.py
+++ coursera.py
@@ -1,11 +1,33 @@
+from lxml import etree
+import requests
+from bs4 import BeautifulSoup
+import openpyxl
+import random
 
 
-def get_courses_list():
-    pass
+def getXML(xml_url):
+    xml_page = requests.get(xml_url)
+    return xml_page
 
 
-def get_course_info(course_slug):
-    pass
+def get_courses_list(xml_page, number_of_urls):
+    all_course_on_cursera=[]
+    root = etree.fromstring(xml_page.content)
+    for element in root.iter():
+        if 'loc' in element.tag:
+            all_course_on_cursera.append(element.text)
+    return random.sample(all_course_on_cursera, number_of_urls)
+
+
+def get_course_info(random_courses_list):
+    for course_url in random_courses_list:
+        course_page = requests.get(course_url)
+        html_page = course_page.text
+        soup = BeautifulSoup(html_page, 'lxml')
+        print(soup.find('h2', attrs={'class':'headline-4-text course-title'}).get_text())
+        print(soup.find('div', attrs={'class':'rc-Language'}).get_text())
+        print(soup.find('div', attrs={'class':'startdate rc-StartDateString caption-text'}).get_text())
+        print(soup.find_all('div', attrs={'class':'week-heading body-2-text'}).get_text())
 
 
 def output_courses_info_to_xlsx(filepath):
@@ -13,4 +35,8 @@ def output_courses_info_to_xlsx(filepath):
 
 
 if __name__ == '__main__':
-    pass
+    xml_url = 'https://www.coursera.org/sitemap~www~courses.xml'
+    number_of_urls = 20
+    xml_page = getXML(xml_url)
+    random_courses_list = get_courses_list(xml_page, number_of_urls)
+    print(get_course_info(random_courses_list))
