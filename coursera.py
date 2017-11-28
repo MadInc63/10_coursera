c4cafedcb2baa9c0a428d780be9df1a44f8afae1
Igor Statkevich <MadInc63@gmail.com>
2017-11-28 21:01:05 +0400

    alpha

--- coursera.py
+++ coursera.py
@@ -20,24 +20,42 @@ def get_courses_list(xml_page, number_of_urls):
 
 
 def get_course_info(random_courses_list):
+    course_info_list = []
     for course_url in random_courses_list:
+        course_info = {}
         course_page = requests.get(course_url)
         html_page = course_page.text
         soup = BeautifulSoup(html_page, 'lxml')
-        print(soup.find('h2', attrs={'class':'headline-4-text course-title'}).get_text())
-        print(soup.find('div', attrs={'class':'rc-Language'}).get_text())
-        print(soup.find('div', attrs={'class':'startdate rc-StartDateString caption-text'}).get_text())
-        print(soup.find_all('div', attrs={'class':'week-heading body-2-text'}).get_text())
+        course_info['title'] = soup.find('h2', attrs={'class':'headline-4-text course-title'}).get_text()
+        course_info['language'] = soup.find('div', attrs={'class':'rc-Language'}).get_text()
+        course_info['date'] = soup.find('div', attrs={'class':'startdate rc-StartDateString caption-text'}).get_text()
+        course_info['week'] = len(soup.find_all('div', attrs={'class':'week-heading body-2-text'}))
+        try:
+            course_info['stars'] = soup.find('div', attrs={'class':'ratings-text bt3-visible-xs'}).get_text()
+        except AttributeError:
+            course_info['stars'] = 'no stars'
+        course_info_list.append(course_info)
+    return course_info_list
 
 
-def output_courses_info_to_xlsx(filepath):
-    pass
+def output_courses_info_to_xlsx(file_path, course_info_list):
+    wb = openpyxl.load_workbook(filename=file_path)
+    sheet = wb['Лист1']
+    row_xlsx = 1
+    for dict in course_info_list:
+        column_xls = 1
+        for key, value in dict.items():
+            sheet.cell(row=row_xlsx, column=column_xls).value = value
+            column_xls +=1
+        row_xlsx += 1
+    wb.save(file_path)
 
 
 if __name__ == '__main__':
     xml_url = 'https://www.coursera.org/sitemap~www~courses.xml'
     number_of_urls = 20
+    file_path = 'coursera.xlsx'
     xml_page = getXML(xml_url)
     random_courses_list = get_courses_list(xml_page, number_of_urls)
-    print(get_course_info(random_courses_list))
-1
+    course_info_list = get_course_info(random_courses_list)
+    output_courses_info_to_xlsx(file_path, course_info_list)
