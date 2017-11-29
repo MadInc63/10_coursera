dc104a6496a3c15fd0e17a393fe966d02127607d
MadInc <MadInc63@gmail.com>
2017-11-29 15:14:49 +0400

    first try

--- README.md
+++ README.md
@@ -1,6 +1,32 @@
 # Coursera Dump
 
-[TODO. There will be project description]
+The script collects information about the courses from the site www.cursera.org and stores the result in a xlsx format file.
+
+# How to use
+
+The script requires the installed Python interpreter version 3.5 To call the help, run the script with the -h or --help option.
+
+```Bash
+python coursera.py -h
+usage: coursera.py [-h] filepath number_of_course
+
+positional arguments:
+  filepath          path to file for load course from www.coursera.org
+  number_of_course  number of random course from www.coursera.org
+
+optional arguments:
+  -h, --help        show this help message and exit
+
+```
+
+To start downloading course information on the website www.cursera.org, you must specify the path to save the file and the number of imported courses through a space after specifying the name of the script.
+
+```Bash
+python coursera.py cours.xlsx 15
+Courses information saved to cours.xlsx
+```
+
+The resulting list of courses will be in the file `cours.xlsx`
 
 # Project Goals
 
--- coursera.py
+++ coursera.py
@@ -1,61 +1,71 @@
 from lxml import etree
-import requests
 from bs4 import BeautifulSoup
-import openpyxl
+from openpyxl import Workbook
+import argparse
+import requests
 import random
 
 
-def getXML(xml_url):
+def arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filepath",
+                        help="path to file for load course "
+                             "from www.coursera.org")
+    parser.add_argument("number_of_course", type=int,
+                        help="number of random course from www.coursera.org")
+    return parser.parse_args()
+
+
+def get_courses_list(xml_url, number_of_urls):
     xml_page = requests.get(xml_url)
-    return xml_page
-
-
-def get_courses_list(xml_page, number_of_urls):
-    all_course_on_cursera=[]
+    all_courses_on_coursera = []
     root = etree.fromstring(xml_page.content)
     for element in root.iter():
         if 'loc' in element.tag:
-            all_course_on_cursera.append(element.text)
-    return random.sample(all_course_on_cursera, number_of_urls)
+            all_courses_on_coursera.append(element.text)
+    return random.sample(all_courses_on_coursera, number_of_urls)
 
 
 def get_course_info(random_courses_list):
-    course_info_list = []
+    courses_info_list = []
     for course_url in random_courses_list:
         course_info = {}
         course_page = requests.get(course_url)
+        course_page.encoding = 'utf-8'
         html_page = course_page.text
-        soup = BeautifulSoup(html_page, 'lxml')
-        course_info['title'] = soup.find('h2', attrs={'class':'headline-4-text course-title'}).get_text()
+        soup = BeautifulSoup(html_page, 'html.parser')
+        course_info['url'] = course_url
+        course_info['title'] = soup.find('h1', attrs={'class':'title display-3-text'}).get_text()
         course_info['language'] = soup.find('div', attrs={'class':'rc-Language'}).get_text()
         course_info['date'] = soup.find('div', attrs={'class':'startdate rc-StartDateString caption-text'}).get_text()
-        course_info['week'] = len(soup.find_all('div', attrs={'class':'week-heading body-2-text'}))
+        course_info['weeks'] = len(soup.find_all('div', attrs={'class':'week-heading body-2-text'}))
         try:
-            course_info['stars'] = soup.find('div', attrs={'class':'ratings-text bt3-visible-xs'}).get_text()
+            course_info['rating'] = soup.find('div', attrs={'class':'ratings-text bt3-visible-xs'}).get_text()
         except AttributeError:
-            course_info['stars'] = 'no stars'
-        course_info_list.append(course_info)
-    return course_info_list
+            course_info['rating'] = 'no rating'
+        courses_info_list.append(course_info)
+    return courses_info_list
 
 
-def output_courses_info_to_xlsx(file_path, course_info_list):
-    wb = openpyxl.load_workbook(filename=file_path)
-    sheet = wb['Лист1']
-    row_xlsx = 1
-    for dict in course_info_list:
-        column_xls = 1
-        for key, value in dict.items():
-            sheet.cell(row=row_xlsx, column=column_xls).value = value
-            column_xls +=1
-        row_xlsx += 1
-    wb.save(file_path)
+def output_courses_info_to_xlsx(filepath, course_info_list):
+    wb = Workbook()
+    ws = wb.active
+    ws.title = 'Coursera'
+    head_table = ['URL', 'Course Title', 'Language', 'Start Date',
+                  'Duration (weeks)', 'Rating']
+    ws.append(head_table)
+    for course in course_info_list:
+        ws.append([course['url'], course['title'], course['language'],
+                   course['date'], course['weeks'], course['rating']])
+    wb.save(filepath)
+    print('Courses information saved to {}'.format(filepath))
 
 
 if __name__ == '__main__':
     xml_url = 'https://www.coursera.org/sitemap~www~courses.xml'
-    number_of_urls = 20
-    file_path = 'coursera.xlsx'
-    xml_page = getXML(xml_url)
-    random_courses_list = get_courses_list(xml_page, number_of_urls)
-    course_info_list = get_course_info(random_courses_list)
-    output_courses_info_to_xlsx(file_path, course_info_list)
+    start_arg = arg_parser()
+    number_of_urls = start_arg.number_of_course
+    filepath = start_arg.filepath
+    random_courses_list = get_courses_list(xml_url, number_of_urls)
+    courses_info_list = get_course_info(random_courses_list)
+    output_courses_info_to_xlsx(filepath, courses_info_list)
